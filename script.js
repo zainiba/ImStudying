3db87e3c75a837f461c1a980bbb9fc22f1380688
Glitch (fin-proj) <none>
2022-01-13 23:06:10 +0000

    🍅🏢 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal(".headline");
+ScrollReveal().reveal(".tagline", { delay: 500 });
+ScrollReveal().reveal(".punchline", { delay: 2000 });
+
+ScrollReveal().reveal(".question1", { delay: 500 });
+
+ScrollReveal().reveal(".insurance", { delay: 200 });
+ScrollReveal().reveal(".insurance1", { delay: 2000 });
+ScrollReveal().reveal(".insurance2", { delay: 4000 });
+ScrollReveal().reveal(".insurance3", { delay: 7000 });
+
+function no() {
+  document.getElementById("demo").innerHTML =
+    "Great! </br> If you are paying out-of-pocket, you will be bumped to the front of the line. </br> Your wait time is 2 minutes. You will recieve the best care possible. </br></br> If you are not paying out-of-pocket, you may qualify for our payment plan with an 8% interest rate.";
+}
+
